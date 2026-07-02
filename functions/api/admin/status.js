2f97726b416cdcc7c39a2a5ca419b6f285e279b4
LimePerrier <matt.lahey@live.com>
2026-07-02 16:45:42 -0230

    Report email config in admin status

--- functions/api/admin/status.js
+++ functions/api/admin/status.js
@@ -4,5 +4,9 @@ export async function onRequestGet({ env }) {
   return json({
     hasAdminInviteToken: Boolean(env.ADMIN_INVITE_TOKEN),
     hasDb: Boolean(env.DB),
+    hasInviteFrom: Boolean(env.INVITE_FROM),
+    hasCloudflareAccountId: Boolean(env.CLOUDFLARE_ACCOUNT_ID),
+    hasCloudflareEmailApiToken: Boolean(env.CLOUDFLARE_EMAIL_API_TOKEN),
+    hasEmailConfig: Boolean(env.INVITE_FROM && env.CLOUDFLARE_ACCOUNT_ID && env.CLOUDFLARE_EMAIL_API_TOKEN),
   });
 }
